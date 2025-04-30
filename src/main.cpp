eb29ff958907b2616dd3ccab90f9b54be797ed20
neverbot <ivan@neverbot.com>
2025-04-30 17:39:15 +0200

    feat: add format argument

--- src/main.cpp
+++ src/main.cpp
@@ -2,42 +2,84 @@
 #include <iostream>
 #include <string>
 
+// enum with the possible formats
+enum class Format {
+  JSON,
+  DSL,
+  DSL_VERBOSE
+};
+
 int main(int argc, char *argv[]) {
-  if (argc < 3 || argc > 4) {
+  if (argc < 4 || argc > 5) {
+    std::cout << "Usage: wadconvert -<format> <wad file> <output json file> "
+                 "[--verbose]\n";
     std::cout
-        << "Usage: wadconvert <wad file> <output json file> [--verbose]\n";
+        << "  -<format>: The format to convert to (-json, -dsl, -dslverbose)\n";
     std::cout << "  wad file: Path to the WAD file to convert\n";
     std::cout << "  output json file: Path to the output JSON file\n";
     std::cout << "  --verbose: Optional flag for detailed output\n";
     return 1;
   }
 
-  std::string wadFilePath  = argv[1];
-  std::string jsonFilePath = argv[2];
-  bool        verbose      = (argc == 4 && std::string(argv[3]) == "--verbose");
+  Format      format;
+  std::string formatStr    = argv[1];
+  std::string wadFilePath  = argv[2];
+  std::string jsonFilePath = argv[3];
+  bool        verbose      = (argc == 5 && std::string(argv[4]) == "--verbose");
+
+  // remove the leading '-' from the format string only if it exists
+  if (formatStr[0] == '-') {
+    formatStr = formatStr.substr(1);
+  }
+
+  if (formatStr == "json") {
+    format = Format::JSON;
+  } else if (formatStr == "dsl") {
+    format = Format::DSL;
+  } else if (formatStr == "dslverbose") {
+    format = Format::DSL_VERBOSE;
+  } else {
+    std::cerr << "Invalid format specified. Use -json, -dsl, or -dslverbose.\n";
+    return 1;
+  }
 
   if (verbose) {
-    std::cout << "Converting WAD file to JSON...\n";
+    std::cout << "Converting WAD file to " << formatStr << " format...\n";
   }
+
   try {
     WAD wad(wadFilePath, verbose);  // Pass verbose flag to WAD constructor
     wad.processWAD();
-    nlohmann::json jsonData = wad.toJSON();
 
-    std::ofstream jsonFile(jsonFilePath);
-    if (!jsonFile) {
-      std::cerr << "Unable to open output JSON file: " << jsonFilePath << "\n";
-      return 1;
+    // Convert WAD data to the proper format depending on the argument passed
+    switch (format) {
+      // convert to JSON
+      case Format::JSON: {
+        nlohmann::json jsonData = wad.toJSON();
+
+        std::ofstream jsonFile(jsonFilePath);
+        if (!jsonFile) {
+          std::cerr << "Unable to open output JSON file: " << jsonFilePath
+                    << "\n";
+          return 1;
+        }
+
+        jsonFile << jsonData.dump(1);
+        jsonFile.close();
+
+        break;
+      }
+
+      default:
+        break;
     }
 
-    jsonFile << jsonData.dump(1);
-    jsonFile.close();
-
     if (verbose) {
-      std::cout << "WAD file converted to JSON successfully.\n";
+      std::cout << "WAD file converted to " << formatStr
+                << " format successfully.\n";
     } else {
       std::cout << std::filesystem::path(wadFilePath).filename().string()
-                << " converted to JSON.\n";
+                << " converted to " << formatStr << ".\n";
     }
   } catch (const std::exception &e) {
     std::cerr << "Error: " << e.what() << "\n";
